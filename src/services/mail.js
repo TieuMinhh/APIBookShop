68cffdcf6edabae57020ec3b5c7cfa2e0d8ae21b
Xiao Ming <minbao4869@gmail.com>
2023-10-20 23:27:23 +0700

    Add Mail

--- src/services/mail.js
+++ src/services/mail.js
@@ -1,31 +1,31 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-let mail = {}
+let mail = {};
 
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'huynhthanhphong12a1@gmail.com',
-        pass: 'ahjzocagubfjvaiu'
-    }
+  service: "gmail",
+  auth: {
+    user: "minbao1412@gmail.com",
+    pass: "dinhbaominh4869",
+  },
 });
 
 mail.sendVerification = (userEmail, verification) => {
-    let mailOptions = {
-        from: 'huynhthanhphong12a1',
-        to: userEmail,
-        subject: 'Xin lại mật khẩu',
-        text: 'Mã xác nhận của bạn là: ' + verification,
-    };
-    transporter.sendMail(mailOptions)
-}
+  let mailOptions = {
+    from: "minbao1412@gmail.com",
+    to: userEmail,
+    subject: "Khôi phục mật khẩu",
+    text: "Mã xác nhận của ngài là : " + verification,
+  };
+  transporter.sendMail(mailOptions);
+};
 
 mail.createCode = () => {
-    let result = '';
-    for (let i = 0; i < 6; i++) {
-        result += String(Math.floor(Math.random() * 10));
-    }
-    return result;
-}
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += String(Math.floor(Math.random() * 10));
+  }
+  return result;
+};
 
-module.exports = mail
+module.exports = mail;
