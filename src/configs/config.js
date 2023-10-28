1907e044a4465adc80268cddcb0cc1f4e96158a3
Xiao Ming <minbao4869@gmail.com>
2023-10-28 23:28:33 +0700

    Update

--- src/configs/config.js
+++ src/configs/config.js
@@ -5,3 +5,4 @@ export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 export const DB_NAME = process.env.DB_NAME || "book_shop";
 export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_URL = process.env.DB_URL || "";
