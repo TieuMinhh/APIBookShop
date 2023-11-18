115b0c70b51a610d6571ad998da3823176eb5165
Xiao Ming <minbao4869@gmail.com>
2023-11-18 16:10:10 +0700

    Update Username

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 8081;
 
 export const DB_HOST = process.env.DB_HOST || "104.197.33.184";
-export const DB_USER = process.env.DB_USER || "XiaoMing";
+export const DB_USER = process.env.DB_USER || "minhalpha";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "123";
 export const DB_NAME = process.env.DB_NAME || "book_shop";
 export const DB_PORT = process.env.DB_PORT || 3306;
