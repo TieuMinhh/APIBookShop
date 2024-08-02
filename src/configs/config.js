6c739802747e10f2de925f34eef1e93358f6ff12
Xiao Ming <minbao4869@gmail.com>
2024-08-02 22:57:42 +0700

    Update

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,13 +1,13 @@
 export const PORT = process.env.PORT || 8081;
 
-export const DB_HOST = process.env.DB_HOST || "104.197.33.184";
-export const DB_USER = process.env.DB_USER || "minhalpha";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "123";
-export const DB_NAME = process.env.DB_NAME || "book_shop";
-export const DB_PORT = process.env.DB_PORT || 3306;
-
-// export const DB_HOST = process.env.DB_HOST || "localhost";
-// export const DB_USER = process.env.DB_USER || "root";
-// export const DB_PASSWORD = process.env.DB_PASSWORD || "";
+// export const DB_HOST = process.env.DB_HOST || "104.197.33.184";
+// export const DB_USER = process.env.DB_USER || "minhalpha";
+// export const DB_PASSWORD = process.env.DB_PASSWORD || "123";
 // export const DB_NAME = process.env.DB_NAME || "book_shop";
 // export const DB_PORT = process.env.DB_PORT || 3306;
+
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "123456";
+export const DB_NAME = process.env.DB_NAME || "book_shop";
+export const DB_PORT = process.env.DB_PORT || 3306;
